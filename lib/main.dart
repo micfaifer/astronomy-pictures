4601993975935a14acbb110c0abe26d217b756d8
Michelle <micfaifer@gmail.com>
2023-08-09 01:17:19 -0300

    Add pull to refresh functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:astronomy_pictures/data/apod_service.dart';
 import 'package:astronomy_pictures/data/data_manager.dart';
 import 'package:astronomy_pictures/viewmodels/apod_viewmodel.dart';
-import 'package:astronomy_pictures/views/widgets/list/apod_list_view.dart';
+import 'package:astronomy_pictures/views/apod_list_screen.dart';
 import 'package:astronomy_pictures/views/widgets/search_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,13 +15,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return ChangeNotifierProvider(
-      create: (context) => ApodViewModel(dataManager: DataManager(ApodService())),
+      create: (context) =>
+          ApodViewModel(dataManager: DataManager(ApodService())),
       child: MaterialApp(
         title: 'NASA APOD App',
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: Scaffold(
+        home: const Scaffold(
           appBar: SearchAppBar(),
           body: ApodListView(),
         ),
---
+++ lib/views/apod_list_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/apod.dart';
+import '../viewmodels/apod_viewmodel.dart';
+import 'widgets/list/apod_list_item.dart';
+
+class ApodListView extends StatefulWidget {
+  const ApodListView({super.key});
+
+  @override
+  _ApodListViewState createState() => _ApodListViewState();
+}
+
+class _ApodListViewState extends State<ApodListView> {
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<ApodViewModel>(context, listen: false).getAstronomyPictures();
+  }
+
+  Future<void> _refreshData(BuildContext context) async {
+    await Provider.of<ApodViewModel>(context, listen: false)
+        .getAstronomyPictures();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ApodViewModel>(
+      builder: (context, viewModel, child) {
+        return RefreshIndicator(
+          onRefresh: () => _refreshData(context),
+          child: Center(
+            child: viewModel.apodList == null
+                ? const CircularProgressIndicator()
+                : ListView.builder(
+                    itemCount: viewModel.apodList?.length,
+                    itemBuilder: (context, index) {
+                      final apodData = viewModel.apodList![index];
+                      return ApodListItem(apodData: apodData);
+                    },
+                  ),
+          ),
+        );
+      },
+    );
+  }
+}
